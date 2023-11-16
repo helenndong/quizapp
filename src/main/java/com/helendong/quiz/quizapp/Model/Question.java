ca82ed158918461f486c242f29dc7bfedf3c9df3
Helen <hd411@nyu.edu>
2023-11-16 13:03:41 -0500

    Add Question entity

--- src/main/java/com/helendong/quiz/quizapp/Model/Question.java
+++ src/main/java/com/helendong/quiz/quizapp/Model/Question.java
@@ -1,4 +1,59 @@
 package com.helendong.quiz.quizapp.Model;
 
+import jakarta.persistence.*;
+
+@Entity
+
 public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String text;
+    private String answer;
+
+    @ManyToOne
+    @JoinColumn(name = "quiz_id")
+    private Quiz quiz;
+
+    public Question() {
+
+    }
+    public Question(String text, String answer, Quiz quiz) {
+        this.text = text;
+        this.answer = answer;
+        this.quiz = quiz;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public Quiz getQuiz() {
+        return quiz;
+    }
+
+    public void setQuiz(Quiz quiz) {
+        this.quiz = quiz;
+    }
 }
