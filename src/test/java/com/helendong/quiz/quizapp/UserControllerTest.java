50d3ab9531de248dd1df99466a9a66ebcb9f5388
Helen <hd411@nyu.edu>
2024-01-12 13:01:12 -0500

    Add unit test for createUser method in UserController

---
+++ src/test/java/com/helendong/quiz/quizapp/UserControllerTest.java
@@ -0,0 +1,54 @@
+package com.helendong.quiz.quizapp;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
+
+import com.helendong.quiz.quizapp.controller.UserController;
+import com.helendong.quiz.quizapp.dto.UserDTO;
+import com.helendong.quiz.quizapp.model.User;
+import com.helendong.quiz.quizapp.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class UserControllerTest {
+
+    @Mock
+    private UserService userService;
+
+    @InjectMocks
+    private UserController userController;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void createUser_SuccessfulCreation_ReturnsCreatedUser() {
+        // Arrange
+        UserDTO userDTO = new UserDTO(1000L, "testUser", "password", "test@email.com");
+        User user = new User();
+        user.setUsername(userDTO.getUsername());
+        user.setPassword(userDTO.getPassword());
+        user.setEmail(userDTO.getEmail());
+
+        when(userService.createUser(any(User.class))).thenReturn(user);
+
+        // Act
+        ResponseEntity<UserDTO> response = userController.createUser(userDTO);
+
+        // Assert
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals("testUser", response.getBody().getUsername());
+        assertEquals("test@email.com", response.getBody().getEmail());
+
+        verify(userService, times(1)).createUser(any(User.class));
+    }
+}
